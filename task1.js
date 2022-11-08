94f64d66e0ac303a3b0630078a9958cb104b52f2
flaviamiranda <flaviagracianom@gmail.com>
2022-11-08 12:28:12 +0000

    task 1 done

--- task1.js
+++ task1.js
@@ -1,3 +1,24 @@
-const cats = ["tony", "daisy", "socks", "rockie" ];
+const cats = ['tony', 'daisy', 'socks', 'rockie']
 
-const capitalisedCats = // your code here
+// TASK 1: 1.a
+
+// const capitalisedCats = function () {
+//   // transform each string to upper case using the toUpperCase() method
+//   // create for loop to interate over array
+//   const result = []
+//   for (let i = 0; i < cats.length; i++) {
+//     // console.log(cats[i].toUpperCase())
+//     result[i] = cats[i].toUpperCase()
+//   }
+//   console.log(result)
+//   return result
+// }
+
+// capitalisedCats()
+
+// ------
+// TASK 1: 1.b
+
+const capitalisedCats = cats.map(function (catName) {
+  console.log(catName.toUpperCase())
+})
